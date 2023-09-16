943702f38ddf10ad742d49422da2499afec4d0a9
HoDucKhanh <hoduckhanh104@gmail.com>
2023-09-16 16:04:26 +0700

    comment

--- Scalapay/src/Frontend/scalapay_fe/src/SurveyComponent.jsx
+++ Scalapay/src/Frontend/scalapay_fe/src/SurveyComponent.jsx
@@ -8,6 +8,7 @@ import { themeJson } from "./theme";
 function SurveyComponent() {
     const survey = new Model(json);
     survey.applyTheme(themeJson);
+    // Attach an event handler to the 'onComplete' event of the survey
     survey.onComplete.add((sender) => {
     console.log(JSON.stringify(sender.data, null, 3));
     const options = {
@@ -18,16 +19,18 @@ function SurveyComponent() {
         },
         body: JSON.stringify(sender.data)
     }
+        // Send an HTTP POST request to the specified URL
         fetch('https://localhost:7183/api/v2/orders', options)
         .then(response => response.json())
         .then(data => {
             if (data.checkoutUrl) {
-              // Chuyển hướng đến checkoutUrl
+              // If the response contains a 'checkoutUrl', redirect the user to that URL
               window.location.href = data.checkoutUrl;
             }
         })
         .catch(err => console.error(err));
     });
+    // Render the survey component with the created survey object
     return (<Survey model={survey} />);
 }
 
